67463889d6c4fc3e80eb5397484fcd157c3d78b3
michael <mbasov@kotelov.team>
2025-10-15 03:52:30 +0300

    feat: api of user

--- apps/microservice/users/src/app.controller.ts
+++ apps/microservice/users/src/app.controller.ts
@@ -1,14 +1,32 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
-import { AddSalaryUserDto, ChangeRoleUserDto, UserTopics } from '@perfume-platform/common';
+import { AddSalaryUserDto, ChangeRoleUserDto, CreateUserDto, EditUserDto, GetAllUsersDto, GetRolesDto, GetSalaryUserDto, GetSalaryUsersDto, GetUserDto, IUserController, RemoveSalaryUserDto, RemoveUserDto, UserTopics } from '@perfume-platform/common';
 import { AddSalaryUserUsecase } from './usecases/add-salary-user/add-salary-user.usecase';
 import { ChangeRoleUserUsecase } from './usecases/change-role-user/change-role-user.usecase';
+import { CreateUserUsecase } from './usecases/create-user/create-user.usecase';
+import { EditUserUsecase } from './usecases/edit-user/edit-user.usecase';
+import { GetAllUsersUsecase } from './usecases/get-all-users/get-all-users.usecase';
+import { GetRolesUsecase } from './usecases/get-roles/get-roles.usecase';
+import { GetSalaryUserUsecase } from './usecases/get-salary-user/get-salary-user.usecase';
+import { GetSalaryUsersUsecase } from './usecases/get-salary-users/get-salary-users.usecase';
+import { GetUserUsecase } from './usecases/get-user/get-user.usecase';
+import { RemoveSalaryUserUsecase } from './usecases/remove-salary-user/remove-salary-user.usecase';
+import { RemoveUserUsecase } from './usecases/remove-user/remove-user.usecase';
 
 @Controller()
-export class AppController {
+export class AppController implements IUserController {
   constructor(
     private readonly addSalaryUserUsecase: AddSalaryUserUsecase,
-    private readonly changeRoleUserUsecase: ChangeRoleUserUsecase
+    private readonly changeRoleUserUsecase: ChangeRoleUserUsecase,
+    private readonly createUserUsecase: CreateUserUsecase,
+    private readonly editUserUsecase: EditUserUsecase,
+    private readonly removeUserUsecase: RemoveUserUsecase,
+    private readonly removeSalaryUserUsecase: RemoveSalaryUserUsecase,
+    private readonly getAllUsersUsecase: GetAllUsersUsecase,
+    private readonly getRolesUsecase: GetRolesUsecase,
+    private readonly getSalaryUserUsecase: GetSalaryUserUsecase,
+    private readonly getSalaryUsersUsecase: GetSalaryUsersUsecase,
+    private readonly getUserUsecase: GetUserUsecase
   ) { }
 
   @MessagePattern(UserTopics.addSalaryUser)
@@ -19,5 +37,50 @@ export class AppController {
   @MessagePattern(UserTopics.changeRoleUser)
   async changeRoleUser(dto: ChangeRoleUserDto) {
     return await this.changeRoleUserUsecase.excecute(dto)
+  }
+
+  @MessagePattern(UserTopics.createUser)
+  async createUser(dto: CreateUserDto) {
+    return await this.createUserUsecase.excecute(dto)
+  }
+
+  @MessagePattern(UserTopics.editUser)
+  async editUser(dto: EditUserDto) {
+    return await this.editUserUsecase.excecute(dto)
+  }
+
+  @MessagePattern(UserTopics.removeUser)
+  async removeUser(dto: RemoveUserDto) {
+    return await this.removeUserUsecase.excecute(dto)
+  }
+
+  @MessagePattern(UserTopics.removeSalaryUser)
+  async removeSalaryUser(dto: RemoveSalaryUserDto) {
+    return await this.removeSalaryUserUsecase.excecute(dto)
+  }
+
+  @MessagePattern(UserTopics.getAllUses)
+  async getAllUsers(dto: GetAllUsersDto) {
+    return await this.getAllUsersUsecase.excecute(dto)
+  }
+
+  @MessagePattern(UserTopics.getRoles)
+  async getRoles(dto: GetRolesDto) {
+    return await this.getRolesUsecase.excecute(dto)
+  }
+
+  @MessagePattern(UserTopics.getSalaryUser)
+  async getSalaryUser(dto: GetSalaryUserDto) {
+    return await this.getSalaryUserUsecase.excecute(dto)
+  }
+
+  @MessagePattern(UserTopics.getSalaryUsers)
+  async getSalaryUsers(dto: GetSalaryUsersDto) {
+    return await this.getSalaryUsersUsecase.excecute(dto)
+  }
+
+  @MessagePattern(UserTopics.getUser)
+  async getUser(dto: GetUserDto) {
+    return await this.getUserUsecase.excecute(dto)
   }
 } 
--- apps/microservice/users/src/app.module.ts
+++ apps/microservice/users/src/app.module.ts
@@ -5,6 +5,15 @@ import { ConfigurationModule } from './modules/config/config.module';
 import { TransportModule } from './modules/transport/transport.module';
 import { AddSalaryUserModule } from './usecases/add-salary-user/add-salary-user.module';
 import { ChangeRoleUserModule } from './usecases/change-role-user/change-role-user.module';
+import { CreateUserModule } from './usecases/create-user/create-user.module';
+import { EditUserModule } from './usecases/edit-user/edit-user.module';
+import { GetAllUsersModule } from './usecases/get-all-users/get-all-users.module';
+import { GetRolesModule } from './usecases/get-roles/get-roles.module';
+import { GetSalaryUserModule } from './usecases/get-salary-user/get-salary-user.module';
+import { GetSalaryUsersModule } from './usecases/get-salary-users/get-salary-users.module';
+import { GetUserModule } from './usecases/get-user/get-user.module';
+import { RemoveSalaryUserModule } from './usecases/remove-salary-user/remove-salary-user.module';
+import { RemoveUserModule } from './usecases/remove-user/remove-user.module';
 
 @Module({
   imports: [
@@ -13,7 +22,16 @@ import { ChangeRoleUserModule } from './usecases/change-role-user/change-role-us
     TransportModule,
 
     AddSalaryUserModule,
-    ChangeRoleUserModule
+    ChangeRoleUserModule,
+    CreateUserModule,
+    EditUserModule,
+    RemoveUserModule,
+    RemoveSalaryUserModule,
+    GetAllUsersModule,
+    GetRolesModule,
+    GetSalaryUserModule,
+    GetSalaryUsersModule,
+    GetUserModule
   ],
   controllers: [AppController],
 })
